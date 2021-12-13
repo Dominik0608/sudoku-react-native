5a2265a31a0c4863d023489d60ee1c32be754643
Dominik <7287876-Dominik987@users.noreply.gitlab.com>
2021-12-13 19:59:38 +0100

    feat: add leaderboard, server communications

---
+++ global.js
@@ -0,0 +1,1 @@
+export const IP = 'http://192.168.0.148/sudoku/';
--- src/Leaderboard.jsx
+++ src/Leaderboard.jsx
@@ -1,11 +1,49 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, KeyboardAvoidingView, ScrollView, RefreshControl } from 'react-native';
+import Constants from "expo-constants";
+import axios from 'axios';
+import { IP } from '../global';
 
 const Leaderboard = () => {
+  const [users, setUsers] = useState([]);
+  const [refreshing] = useState(false);
+
+  const updateLeaderboard = async () => {
+    axios.get(IP + 'leaderboard')
+      .then(function (response) {
+        setUsers(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  useEffect(() => {
+    updateLeaderboard();
+  }, [])
+
   return(
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Ranglista</Text>
-    </View>
+    <KeyboardAvoidingView
+      behavior={ Platform.OS == "ios" ? "padding" : "height" }
+      style={{ marginTop: Constants.statusBarHeight, padding: 10 }}
+    >
+      <ScrollView
+        contentContainerStyle={{ flex: 1 }}
+        refreshControl={
+          <RefreshControl
+            refreshing={refreshing}
+            onRefresh={updateLeaderboard}
+          />
+        }
+      >
+        <View style={{ alignItems: 'center' }}>
+          <Text style={{ fontSize: 26, marginBottom: 5, paddingHorizontal: 20, borderBottomWidth: 2, borderColor: '#0c63f0' }}>Játékot teljesítők</Text>
+          {users.map((user, index) => (
+            <Text key={user.id} style={{ fontSize: 20, backgroundColor: (index % 2 ? "#ccc" : "#ddd"), width: '100%', textAlign: 'center', padding: 5 }}>{user.name}</Text>
+          ))}
+        </View>
+      </ScrollView>
+    </KeyboardAvoidingView>
   )
 }
 
