4db9861e66b98efdbcd5b2e96d409f165f3cd78c
Dominik0608 <59706770+Dominik0608@users.noreply.github.com>
2021-12-13 20:00:16 +0100

    feat: add leaderboard, server communications (#2)
    Co-authored-by: Dominik <7287876-Dominik987@users.noreply.gitlab.com>

--- src/Sudoku.jsx
+++ src/Sudoku.jsx
@@ -1,27 +1,30 @@
 import React, { useState, useEffect, useReducer } from 'react';
-import { View, Text, Modal, ScrollView, FlatList, KeyboardAvoidingView, Dimensions, TouchableOpacity } from 'react-native';
+import { View, Text, Modal, KeyboardAvoidingView, Dimensions, TouchableOpacity } from 'react-native';
 import { TextInput, Button } from 'react-native-paper';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
 import Constants from "expo-constants";
+import Toast from 'react-native-toast-message';
+import axios from 'axios';
+import { IP } from '../global';
 
 const Sudoku = () => {
   const [modal, setModal] = useState(false);
   const [username, setUsername] = useState('');
   const [values, setValues] = useState([
-    [[1, true], [2, true], [3, true], [4, true], [5, true], [6, true], [7, true], [8, true], [9, true]],
-    [[1, false], [2, true], [3, true], [4, true], [5, true], [6, true], [7, true], [8, true], [9, true]],
-    [[1, true], [2, true], [3, false], [4, true], [5, true], [6, true], [7, true], [8, true], [9, true]],
-    [[1, true], [2, true], [3, true], [4, true], [5, false], [6, true], [7, true], [8, true], [9, true]],
-    [[1, true], [2, true], [3, true], [4, true], [5, true], [6, true], [7, true], [8, true], [9, true]],
-    [[1, true], [2, false], [3, true], [4, true], [5, true], [6, true], [7, false], [8, true], [9, true]],
-    [[1, true], [2, true], [3, true], [4, true], [5, true], [6, true], [7, true], [8, true], [9, true]],
-    [[1, false], [2, true], [3, true], [4, true], [5, true], [6, true], [7, true], [8, true], [9, true]],
-    [[1, true], [2, true], [3, true], [4, false], [5, true], [6, true], [7, true], [8, true], [9, false]],
+    [[1, false], [2, false], [3, false], [4, false], [5, false], [6, false], [7, false], [8, false], [9, false]],
+    [[1, false], [2, false], [3, false], [4, false], [5, false], [6, false], [7, false], [8, false], [9, false]],
+    [[1, false], [2, false], [3, false], [4, false], [5, false], [6, false], [7, false], [8, false], [9, false]],
+    [[1, false], [2, false], [3, false], [4, false], [5, false], [6, false], [7, false], [8, false], [9, false]],
+    [[1, false], [2, false], [3, false], [4, false], [5, false], [6, false], [7, false], [8, false], [9, false]],
+    [[1, false], [2, false], [3, false], [4, false], [5, false], [6, false], [7, false], [8, false], [9, false]],
+    [[1, false], [2, false], [3, false], [4, false], [5, false], [6, false], [7, false], [8, false], [9, false]],
+    [[1, false], [2, false], [3, false], [4, false], [5, false], [6, false], [7, false], [8, false], [9, false]],
+    [[1, false], [2, false], [3, false], [4, false], [5, false], [6, false], [7, false], [8, false], [9, false]],
   ]);
+  const [selected, setSelected] = useState(1);
+
   const [, forceUpdate] = useReducer((x) => !x, false)
 
-  const [selected, setSelected] = useState(1);
   const windowWidth = Dimensions.get('window').width - 20;
 
   useEffect(() => {
@@ -42,6 +45,7 @@ const Sudoku = () => {
   });
 
   const setUser = async () => {
+    if (username.length === 0) { return false; }
     try {
       await AsyncStorage.setItem('username', username);
       setModal(false);
@@ -49,6 +53,29 @@ const Sudoku = () => {
       setUsername('Ismeretlen');
     }
   }
+
+  const generateSudoku = async () => {
+    axios.get(IP + 'generate')
+      .then(function (response) {
+        let matrix = response.data;
+        for (let i = 0; i < matrix.length; i++) {
+          const row = matrix[i];
+          for (let j = 0; j < row.length; j++) {
+            const cell = row[j];
+            row[j] = [cell, !cell];
+          }
+        }
+        setValues(matrix);
+        console.log('sudoku generated');
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  useEffect(() => {
+    generateSudoku();
+  }, [])
 
   const changeValue = (row, column) => {
     let newValues = values;
@@ -59,71 +86,105 @@ const Sudoku = () => {
     forceUpdate(); // ha már máshogy nem akar frissülni a componens
   }
 
+  const checkSudokuSolve = async () => {
+    Toast.hide();
+    axios.post(IP + 'checker', {
+      username: username,
+      sudoku: values
+    })
+    .then(function (response) {
+      console.log(response.data);
+      if (response.data) {
+        generateSudoku();
+        Toast.show({
+          type: 'success',
+          text1: 'Gratulálok, sikeres megoldás!',
+          visibilityTime: 5000,
+        });
+      } else {
+        Toast.show({
+          type: 'error',
+          text1: 'Sajnos nem sikerült helyesen megoldani.',
+          visibilityTime: 5000,
+        });
+      }
+    })
+    .catch(function (error) {
+      Toast.show({
+        type: 'error',
+        text1: 'Hiba történt a csatlakozás során!',
+        text2: 'Kérlek próbálkozz később!',
+        visibilityTime: 5000,
+      });
+    });
+  }
+
   return(
     <KeyboardAvoidingView
       behavior={ Platform.OS == "ios" ? "padding" : "height" }
       style={{ marginTop: Constants.statusBarHeight, height: "100%", padding: 10 }}
     >
+      <View style={{ flex: 1,  alignItems: 'center' }}>
+        <Text style={{ margin: 20, fontSize: 26 }}>Helló, <Text style={{ color: "#0c63f0" }}>{username}</Text>!</Text>
 
-    <SafeAreaProvider>
-      {/* <ScrollView contentContainerStyle={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}> */}
-        <View style={{ flex: 1,  alignItems: 'center' }}>
-          <Text style={{ margin: 20, fontSize: 26 }}>Helló, {username}!</Text>
-
-          <View>
-            {values.map((row, rowIndex) => (
-              <View key={rowIndex} style={{ flexDirection: 'row', borderBottomWidth: ((rowIndex === 2 || rowIndex === 5 ) ? 3 : 1) }}>
-                {row.map((cell, cellIndex) => (
-                  <TouchableOpacity key={cellIndex} activeOpacity={cell[1] ? 0.2 : 1} onPress={ () => changeValue(rowIndex, cellIndex) }>
-                    <View style={{ height: windowWidth/9, width: windowWidth/9, borderWidth: 1, borderRightWidth: ((cellIndex === 2 || cellIndex === 5) ? 3 : 1), borderColor: '#000', alignItems: 'center', justifyContent: 'center', backgroundColor: (cell[1] ? "#fff" : "#ccc") }}>
-                      <Text style={{ fontSize: windowWidth/15, color: (cell[1] ? "#000" : "#f00") }} allowFontScaling={false}>{cell[0]}</Text>
-                    </View>
-                  </TouchableOpacity>
-                ))}
-              </View>
-            ))}
-          </View>
-
-          <View style={{ flexDirection: 'row', marginVertical: 10 }}>
-            {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((cell, cellIndex) => (
-              <TouchableOpacity key={cellIndex} onPress={ () => setSelected(cell) }>
-                <View style={{ height: windowWidth/9, width: windowWidth/9, borderWidth: 1, borderColor: '#000', alignItems: 'center', justifyContent: 'center', backgroundColor: selected === cell ? '#6eb9ff' : '#fff' }}>
-                  <Text style={{ fontSize: windowWidth/15 }} allowFontScaling={false}>{cell}</Text>
-                </View>
-              </TouchableOpacity>
-            ))}
-          </View>
-
-          <Button mode="contained" onPress={() => console.log('asd')}>
-            Ellenőrzés
-          </Button>
-
+        <View>
+          {values.map((row, rowIndex) => (
+            <View key={rowIndex} style={{ flexDirection: 'row', borderBottomWidth: ((rowIndex === 2 || rowIndex === 5 ) ? 3 : 1) }}>
+              {row.map((cell, cellIndex) => (
+                <TouchableOpacity key={cellIndex} activeOpacity={cell[1] ? 0.2 : 1} onPress={ () => changeValue(rowIndex, cellIndex) }>
+                  <View style={{ height: windowWidth/9, width: windowWidth/9, borderWidth: 1.1, margin: -0.1, borderRightWidth: ((cellIndex === 2 || cellIndex === 5) ? 3 : 1), borderColor: '#000', alignItems: 'center', justifyContent: 'center', backgroundColor: (cell[1] ? "#fff" : "#ccc") }}>
+                    <Text style={{ fontSize: windowWidth/15, color: (cell[1] ? "#000" : "#f00") }} allowFontScaling={false}>{cell[0]}</Text>
+                  </View>
+                </TouchableOpacity>
+              ))}
+            </View>
+          ))}
         </View>
 
-        <Modal
-          animationType="slide"
-          transparent={false}
-          visible={modal}
-          onRequestClose={() => {
-            setModal(!modal);
-          }}
-        >
-          <View style={{ flex: 1, justifyContent: 'center' }}>
-            <View style={{ margin: 20 }}>
-              <Text style={{ textAlign: 'center' }}>Üdvözöllek kedves idegen!</Text>
-              <Text style={{ textAlign: 'center' }}>Kérlek add meg a neved a játékhoz:</Text>
-              <TextInput
-                value={username}
-                onChangeText={text => setUsername(text)}
-              />
-              <Button mode="contained" onPress={() => setUser()}>
-                Tovább
-              </Button>
-            </View>
+        <View style={{ flexDirection: 'row', marginVertical: 10 }}>
+          {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((cell, cellIndex) => (
+            <TouchableOpacity key={cellIndex} onPress={ () => setSelected(cell) }>
+              <View style={{ height: windowWidth/9, width: windowWidth/9, borderWidth: 1, borderColor: '#000', alignItems: 'center', justifyContent: 'center', backgroundColor: selected === cell ? '#7ec9ff' : '#fff' }}>
+                <Text style={{ fontSize: windowWidth/15 }} allowFontScaling={false}>{cell}</Text>
+              </View>
+            </TouchableOpacity>
+          ))}
+        </View>
+
+        <Button mode="contained" onPress={() => checkSudokuSolve()}>
+          Ellenőrzés
+        </Button>
+
+      </View>
+
+      <Modal
+        animationType="slide"
+        transparent={false}
+        visible={modal}
+        onRequestClose={() => {
+          return false;
+        }}
+      >
+        <View style={{ flex: 1, justifyContent: 'center' }}>
+          <View style={{ margin: 20 }}>
+            <Text style={{ textAlign: 'center', fontSize: 22 }}>
+              Üdvözöllek kedves idegen!
+              {"\n"}
+              Kérlek add meg a neved a játékhoz:
+            </Text>
+            <TextInput
+              value={username}
+              onChangeText={text => setUsername(text)}
+              maxLength={40}
+              style={{ textAlign: 'center', marginVertical: 10 }}
+            />
+            <Button mode="contained" onPress={() => setUser()}>
+              Tovább
+            </Button>
           </View>
-        </Modal>
-      {/* </ScrollView> */}
-    </SafeAreaProvider>
+        </View>
+      </Modal>
+      <Toast />
     </KeyboardAvoidingView>
   )
 }
